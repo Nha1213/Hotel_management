068bd127626f0e5e7c0b3cb34e1e3314cdb1f3d3
Nha1213 <Vothanarern@gmail.com>
2026-09-01 22:58:03 +0700

    update route permission

--- project_reactJs/src/App.jsx
+++ project_reactJs/src/App.jsx
@@ -1,23 +1,23 @@
-import { Routes, Route, } from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 
 import Layout from "./Page/components/Layout";
 import Login from "./Page/backend/user_account/Login";
 import Register from "./Page/backend/user_account/Register";
 import Dashboard from "./Page/backend/dashboard/Dashboard";
-import RoomType from './Page/backend/roomType/RoomType';
+import RoomType from "./Page/backend/roomType/RoomType";
 import Room from "./Page/backend/Room/Room";
 import Branches from "./Page/backend/branches/Branches";
 import Protect from "./Page/backend/Protect/Protect";
 
 import LayoutPage from "./Page/frontend/layout/LayoutPage";
 import Homepage from "./Page/frontend/Homepage/Homepage";
-import AboutPage from './Page/frontend/AboutPage/AboutPage';
+import AboutPage from "./Page/frontend/AboutPage/AboutPage";
 
 const App = () => {
   return (
     <Routes>
-      <Route element={<Layout />}>
-        <Route element={<Protect />}>
+      <Route element={<Protect />}>
+        <Route element={<Layout />}>
           <Route path="/" element={<Dashboard />} />
           <Route path="/room" element={<Room />} />
           <Route path="/room_type" element={<RoomType />} />
@@ -29,11 +29,11 @@ const App = () => {
       <Route path="/register" element={<Register />} />
 
       <Route element={<LayoutPage />}>
-        <Route path='/index' element={<Homepage />} />
-        <Route path='/about' element={<AboutPage />} />
+        <Route path="/index" element={<Homepage />} />
+        <Route path="/about" element={<AboutPage />} />
       </Route>
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
